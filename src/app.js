82af8296d9880f1a8c16a6aa86b3dfd51e7224af
Ben Townsend <btownsend220@gmail.com>
2015-07-01 16:45:19 +0100

    object created for each reel with reference to node. spin function is now a method of each of these objects.

--- src/app.js
+++ src/app.js
@@ -1,31 +1,39 @@
 (function() {
 
-  var lever = document.getElementById('lever');
-  lever.addEventListener('click', function() {
-    var leverSound = new Audio('../src/sounds/lever.mp3').play();
-    setTimeout(spin, 1000);
-  });
+  function Reel(element) {
+    this.element = element;
+  }
+  Reel.prototype.spin = function spin() {
+    var that = this.element;
+    that.style.bottom = (that.style.bottom.split('px').shift() - 100) + 'px';
+    if (that.style.bottom.split('px').shift() > 0) {
+      window.requestAnimationFrame(this.spin.bind(this));
+    } else {
+      that.style.bottom = reelStart;
+      window.requestAnimationFrame(this.spin.bind(this));
+    }
+  }
 
   // Select all reels as a node list
   var reels = document.querySelectorAll('.reel');
-  // Select reels individually
-  var reel1 = reels[0],
-      reel2 = reels[1],
-      reel3 = reels[2];
+  // Create a new object for each reel, with a reference to the node in the 'element' property
+  var reel1 = new Reel(reels[0]);
+  var reel2 = new Reel(reels[1]);
+  var reel3 = new Reel(reels[2]);
   var reelStart = 1410 + 'px';
   // Set initial position of reels
   for (let i = 0; i < reels.length; i++) {
     reels[i].style.bottom = reelStart;
   }
 
-  function spin() {
-    reel1.style.bottom = (reel1.style.bottom.split('px').shift() - 100) + 'px';
-    if (reel1.style.bottom.split('px').shift() > 0) {
-      window.requestAnimationFrame(spin);
-    } else {
-      reel1.style.bottom = reelStart;
-      window.requestAnimationFrame(spin);
-    }
-  }
+  var lever = document.getElementById('lever');
+  lever.addEventListener('click', function() {
+    var leverSound = new Audio('../src/sounds/lever.mp3').play();
+    setTimeout(function() {
+      reel1.spin();
+      reel2.spin();
+      reel3.spin();
+    }, 1000);
+  });
 
 })();
