a0fd2b6ce560b9063e38d6c751671f64b9742493
Ben Townsend <btownsend220@gmail.com>
2015-07-01 13:17:18 +0100

    function to query element style removed. velocity of reel spin increased. sound effect added for lever pull

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 (function() {
 
-  function getStyle(prop, element) {
-    return window.getComputedStyle(element).getPropertyValue(prop); 
-  }
-
   var lever = document.getElementById('lever');
-  lever.addEventListener('click', spin);
+  lever.addEventListener('click', function() {
+    var leverSound = new Audio('../src/sounds/lever.mp3').play();
+    setTimeout(spin, 1000);
+  });
+
   var reel1 = document.querySelector('.reel-1');
 
   // Set initial position of reel
   var reel1Start = reel1.style.bottom = 1410 + 'px';
 
   function spin() {
-    reel1.style.bottom = (reel1.style.bottom.split('px').shift() - 20) + 'px';
+    reel1.style.bottom = (reel1.style.bottom.split('px').shift() - 100) + 'px';
     if (reel1.style.bottom.split('px').shift() > 0) {
       window.requestAnimationFrame(spin);
     } else {
