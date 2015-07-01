f24baa21506d66a9061898c488b051e3461a1ea6
Ben Townsend <btownsend220@gmail.com>
2015-07-01 15:13:23 +0100

    1 variable declared for reels as a group, and 3 more for the reels individually. css 'bottom' property applied to all reels on page load

--- src/app.js
+++ src/app.js
@@ -6,17 +6,24 @@
     setTimeout(spin, 1000);
   });
 
-  var reel1 = document.querySelector('.reel-1');
-
-  // Set initial position of reel
-  var reel1Start = reel1.style.bottom = 1410 + 'px';
+  // Select all reels as a node list
+  var reels = document.querySelectorAll('.reel');
+  // Select reels individually
+  var reel1 = reels[0],
+      reel2 = reels[1],
+      reel3 = reels[2];
+  var reelStart = 1410 + 'px';
+  // Set initial position of reels
+  for (let i = 0; i < reels.length; i++) {
+    reels[i].style.bottom = reelStart;
+  }
 
   function spin() {
     reel1.style.bottom = (reel1.style.bottom.split('px').shift() - 100) + 'px';
     if (reel1.style.bottom.split('px').shift() > 0) {
       window.requestAnimationFrame(spin);
     } else {
-      reel1.style.bottom = reel1Start;
+      reel1.style.bottom = reelStart;
       window.requestAnimationFrame(spin);
     }
   }
